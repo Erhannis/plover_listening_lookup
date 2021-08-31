61dc13cd1219e8cfb0412d2afffd89e23014b184
Kaoffie <kaoffie@gmail.com>
2021-09-01 00:16:26 +0800

    Removed dataclasses

--- plover_next_stroke/next_stroke_config.py
+++ plover_next_stroke/next_stroke_config.py
@@ -1,12 +1,24 @@
-from dataclasses import dataclass, replace
-
 from plover_next_stroke.sorting import SortingType
 
-@dataclass
+
+CONFIG_ITEMS = {
+    "row_height": 30,
+    "page_len": 10,
+    "sorting_type": SortingType.FREQUENCY
+}
+
+
 class NextStrokeConfig:
-    row_height: int = 30
-    page_len: int = 10
-    sorting_type: SortingType = SortingType.FREQUENCY
+    def __init__(self, values: dict = None):
+        if values is None:
+            values = dict()
+
+        for key, default in CONFIG_ITEMS.items():
+            if key in values:
+                setattr(self, key, values[key])
+            else:
+                setattr(self, key, default)
 
     def copy(self) -> "NextStrokeConfig":
-        return replace(self)
+        value_dict = {k: getattr(self, k) for k in CONFIG_ITEMS.keys()}
+        return NextStrokeConfig(value_dict)
