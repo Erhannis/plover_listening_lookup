4538e913597f3fa1a7a7c34c8d1f5193445e6834
erhannis <eyeillus@gmail.com>
2025-01-06 21:23:44 -0500

    Converted plover_next_stroke to plover_listening_lookup.  (I was using a different computer or I'd have made far more commits.)

--- plover_listening_lookup/listening_lookup_config.py
+++ plover_listening_lookup/listening_lookup_config.py
@@ -1,14 +1,11 @@
-from plover_next_stroke.sorting import SortingType
 
 
 CONFIG_ITEMS = {
-    "row_height": 30,
-    "page_len": 10,
-    "sorting_type": SortingType.FREQUENCY
+    "list_len": 3
 }
 
 
-class NextStrokeConfig:
+class ListeningLookupConfig:
     def __init__(self, values: dict = None):
         if values is None:
             values = dict()
@@ -19,6 +16,6 @@ class NextStrokeConfig:
             else:
                 setattr(self, key, default)
 
-    def copy(self) -> "NextStrokeConfig":
+    def copy(self) -> "ListeningLookupConfig":
         value_dict = {k: getattr(self, k) for k in CONFIG_ITEMS.keys()}
-        return NextStrokeConfig(value_dict)
+        return ListeningLookupConfig(value_dict)
